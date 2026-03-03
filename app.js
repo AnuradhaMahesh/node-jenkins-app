727b64d62588cd6f25c36aa5c6135295a13b57eb
Anuradha <2025.anuradha.mahesh@ves.ac.in>
2026-03-03 19:55:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("NodeJS Application Running Successfully");
+//console.log("NodeJS Application Running Successfully");
+
+
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('NodeJS Application Deployed Successfully!');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
